aa3d59ba44c0c21cce9a3aa1cf2a575f5deae18f
Ale Aguiar Díaz <122371223+aleaguiard@users.noreply.github.com>
2024-06-16 21:53:48 +0100

    feat: add database and show datetime messages

--- client/js/client.js
+++ client/js/client.js
@@ -4,6 +4,8 @@ const socket = io();
 
 const input = document.getElementById("input");
 const form = document.getElementById("form");
+const now = new Date();
+const formattedDateTime = now.toLocaleString();
 
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
@@ -18,5 +20,13 @@ socket.on("message", (msg) => {
 	// messages.insertAdjacentHTML("beforeend", item);
 	const li = document.createElement("li");
 	li.textContent = msg;
+
+	const small = document.createElement("small");
+	small.textContent = formattedDateTime;
+
+	small.style.fontSize = "0.5rem";
+	small.style.color = "#999";
+
+	li.appendChild(small);
 	document.getElementById("messages").appendChild(li);
 });
--- server/index.js
+++ server/index.js
@@ -1,12 +1,31 @@
 import express from "express";
 import logger from "morgan";
+import dotenv from "dotenv";
+import { createClient } from "@libsql/client";
+
 import { Server } from "socket.io";
 import { createServer } from "node:http";
 
+dotenv.config();
 const app = express();
 const server = createServer(app);
 const io = new Server(server, { connectionStateRecovery: true });
+
+const db = createClient({
+	url: process.env.DATABASE_URL,
+	authToken: process.env.DATABASE_AUTH_TOKEN,
+});
+
+await db.execute(
+	`CREATE TABLE IF NOT EXISTS messages (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	content TEXT,
+	date DATETIME DEFAULT CURRENT_TIMESTAMP
+)`
+);
+
 const port = process.env.PORT ?? 3000;
+
 app.use(logger("dev"));
 app.use(express.static("client"));
 
@@ -15,7 +34,19 @@ io.on("connection", (socket) => {
 	socket.on("disconnect", () => {
 		console.log("A user has disconnected");
 	});
-	socket.on("message", (msg) => {
+	socket.on("message", async (msg) => {
+		let message;
+		try {
+			const now = new Date();
+			const formattedDate = `${now.toLocaleTimeString()} ${now.toLocaleDateString()}`;
+			message = await db.execute({
+				sql: `INSERT INTO messages (content, date) VALUES (:msg, :date)`,
+				args: { msg, date: formattedDate },
+			});
+		} catch (e) {
+			console.log(e);
+			return;
+		}
 		io.emit("message", msg);
 	});
 });
